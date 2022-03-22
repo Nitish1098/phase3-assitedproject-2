41ac002075c526052b55ca94bb734fccc1080816
Nitish1098 <98463564+Nitish1098@users.noreply.github.com>
2022-03-22 12:10:26 +0530

    Add files via upload

---
+++ Feedbackapplication.java
@@ -0,0 +1,15 @@
+
+
+//SpringBootApplication
+
+public class Feedbackapplication {
+
+	public static void main(String[] args) {
+
+		Object SpringApplication;
+
+		((Object) SpringApplication).run(Feedbackapplication.class, args);
+}
+
+}
+
